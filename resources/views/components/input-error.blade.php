96b5b3ded38fef6262e8a6dbab77236dc0280bdd
hannd15 <lejhernandez08@gmail.com>
2024-05-27 08:49:56 -0500

    Mensajes de error de breeze más claros Me equivocé de rama xd

--- resources/views/components/input-error.blade.php
+++ resources/views/components/input-error.blade.php
@@ -3,7 +3,12 @@
 @if ($messages)
     <ul {{ $attributes->merge(['class' => 'text-sm text-red-600 space-y-1']) }}>
         @foreach ((array) $messages as $message)
-            <li>{{ $message }}</li>
+        @if($message == "auth.failed")
+            <li>Credenciales incorrectas</li>
+        @endif
+        @if ($message == "validation.unique")
+            <li>Este correo ya existe</li>
+        @endif
         @endforeach
     </ul>
 @endif
